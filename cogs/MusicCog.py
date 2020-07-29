0234592f65c04d2b669e08a626795b6b16fddf62
deadinteger <deadinteger@gmail.com>
2020-07-29 12:52:30 -0400

    Figuring out current directory

--- cogs/MusicCog.py
+++ cogs/MusicCog.py
@@ -27,6 +27,7 @@ class MusicCog(commands.Cog):
 
     @commands.command()
     async def play(self, ctx, url):
+        queue = []
         song_there = os.path.isfile("song.mp3")
         try:
             if song_there:
@@ -40,7 +41,8 @@ class MusicCog(commands.Cog):
         await ctx.send("Obtaining music")
         server = ctx.message.author.guild
         voice = ctx.message.author.guild.voice_client
-        print(type(voice))
+        name = ""
+        print(os.getcwd())
         ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
@@ -53,7 +55,6 @@ class MusicCog(commands.Cog):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             print("Downloading audio now\n")
             ydl.download([url])
-        name = ""
         for file in os.listdir("./"):
             if file.endswith(".mp3"):
                 name = file
@@ -63,75 +64,15 @@ class MusicCog(commands.Cog):
             voice.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: print('done', e))
         except AttributeError:
             ctx.send("The bot is not in a voice channel, use []join")
+        except discord.errors.ClientException:
+            queue.append(url)
         voice.source = discord.PCMVolumeTransformer(voice.source)
         voice.source.volume = 0.07
         nname = name.rsplit("-", 2)
         await ctx.send(f"Playing: {nname[0]}")
         print("playing\n")
 
-    @commands.command()
-    async def play2(self, ctx, url):
-        def check_queue():
-            Queue_infile = os.path.isdir("./Queue")
-            if Queue_infile is True:
-                DIR = os.path.abspath(os.path.realpath("Queue"))
-                length = len(os.listdir(DIR))
-                still_q = length - 1
-                try:
-                    first_file = os.listdir()[0]
-                except:
-                    print("No more queued song(s)\n")
-                    asyncio.queues.clear()
-                    return
-                main_location = os.path.dirname(os.path.realpath(__file__))
-                song_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
-                if length != 0:
-                    print("Song done, playing next queued\n")
-                    print(f"Songs still in queue: {still_q}")
-                    song_there = os.path.isfile("song.mp3")
-                    if(song_there):
-                        os.remove("song.mp3")
-                    shutil.move(song_path, main_location)
-                    for file in os.listdir("./"):
-                        if file.endswith(".mp3"):
-                            os.rename(file, 'song.mp3')
-
-                    server = ctx.message.author.guild
-                    voice = server.voice_client
-
-                    voice.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: check_queue())
-                    voice.source = discord.PCMVolumeTransformer(voice.source)
-                    voice.source.volume = 0.07
-                else:
-                    asyncio.queues.clear()
-                    return
-            else:
-                asyncio.queues.clear()
-                print("No songs were queued before the ending of the last song\n")
-        song_there = os.path.isfile("song.mp3")
-        try:
-            if song_there:
-                os.remove("song.mp3")
-                asyncio.queues.clear()
-                print("Removed old song file")
-        except PermissionError:
-            print("Trying to delete song file, but it's being played")
-            await ctx.send("Error: Music Playing")
-            return
-        Queue_infile = os.path.isdir("./Queue")
-        try:
-            Queue_folder = "./Queue"
-            if Queue_infile is True:
-                print("Removed old Queue Folder")
-                shutil.rmtree(Queue_folder)
-        except:
-            print("No old queue folder")
-
-        await ctx.send("Getting everything ready now")
-
-        server = ctx.message.author.guild
-        voice = server.voice_client
-
+    def downloadSong(self, url):
         ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
@@ -144,21 +85,6 @@ class MusicCog(commands.Cog):
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             print("Downloading audio now\n")
             ydl.download([url])
-
-        for file in os.listdir("./"):
-            if file.endswith(".mp3"):
-                name = file
-                print(f"Renamed File: {file}\n")
-                os.rename(file, "song.mp3")
-
-        voice.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: check_queue())
-        voice.source = discord.PCMVolumeTransformer(voice.source)
-        voice.source.volume = 0.07
-
-        nname = name.rsplit("-", 2)
-        await ctx.send(f"Playing: {nname[0]}")
-        print("playing\n")
-
 
     @commands.command
     async def pause(self, ctx):
