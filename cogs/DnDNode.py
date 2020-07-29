cda3e9f1e805060b54cab13b505d159e83c4731e
deadinteger <deadinteger@gmail.com>
2020-07-29 09:17:22 -0400

    Added some basic dnd functions

--- cogs/DnDNode.py
+++ cogs/DnDNode.py
@@ -33,6 +33,25 @@ class DnDNode(commands.Cog):
                 else:
                     print("Path creation success!")
 
+    @commands.command()
+    async def spells(self, ctx):
+        await ctx.send("https://donjon.bin.sh/5e/spells/")
+
+    @commands.command()
+    async def classes(self, ctx):
+        await ctx.send("http://dnd5e.wikidot.com/#toc5")
+
+    @commands.command()
+    async def names(self, ctx):
+        await ctx.send("https://www.fantasynamegenerators.com/")
+
+    @commands.command()
+    async def roll(self, ctx, dice):
+        if dice == 4 | dice == 6 | dice == 8 | dice == 12 | dice == 20:
+            await ctx.send(random.randint(1, dice))
+        else:
+            await ctx.send("Please use a valid 5e dice type, 4, 6, 8, 12 and 20 \n Ex: /roll 4")
+
 
 def setup(client):
     client.add_cog(DnDNode(client))
